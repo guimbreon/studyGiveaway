70d77adff4abc7dcba50bd8455ebbfe4b820e865
guimbreon <gui.acs26@gmail.com>
2023-12-26 16:46:19 +0000

    version 1.2: Now you can personalize the mininum age, the max age and the medianGrade; There are personalized errors for: minAge > maxAge; Trying to personalize with 4 arguments and there being only 3 possible personalizations; not giving/giving an invalid filePath.

--- refresh.py
+++ refresh.py
@@ -20,8 +20,11 @@ def refresh(fileName, minAge = MINAGE, maxAge = MAXAGE, medGrade = MEDGRADE):
     """
     newFile = copy.deepcopy(fileName).replace("data","lastData")
     file = dataWork.readFile(fileName)
+    print("a")
     people = dataWork.selection(file, minAge , maxAge)
+    print("b")
     people = dataWork.medianGrade(people, medGrade)
+    print("c")
     cities = dataWork.getCities(people)
     allTop5 = []
     for city in cities:
@@ -33,7 +36,25 @@ def refresh(fileName, minAge = MINAGE, maxAge = MAXAGE, medGrade = MEDGRADE):
     dataWork.writeFile(newFile, allInOne)
     print("finished")
 try:
+    
     fileName = sys.argv[1]
-    refresh(fileName)
+    custom = len(sys.argv)
+    print(custom)
+    if custom <= 5:
+        if custom == 2:
+            refresh(fileName)
+        elif custom == 3:
+            refresh(fileName, int(sys.argv[2]), 122) # 122 being the age of the oldest person to ever live.
+        if sys.argv[2] <= sys.argv[3]:
+            if custom == 4:
+                refresh(fileName, int(sys.argv[2]), int(sys.argv[3])) 
+            elif custom == 5:
+                refresh(fileName, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])) 
+        else:
+            print("The amount given in minAge should be smaller than maxAge!")
+    else:
+        print("""The amount of arguments given for custom refresh is greater than 3.
+              python3 refresh.py filePath minAge maxAge medGrade
+              This is the biggest run you should do.""")
 except:
     print("Enter a valid filePath in the format\npython3 refresh.py filePath")
